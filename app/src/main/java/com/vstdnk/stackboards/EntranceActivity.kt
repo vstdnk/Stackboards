b64091df321985f07b8c98f2abea8a1c9097d988
Vladimir Stadnik <va.stdnk@gmail.com>
2024-07-23 20:50:28 +0300

    Added entrance/login activity

--- app/src/main/java/com/vstdnk/stackboards/EntranceActivity.kt
+++ app/src/main/java/com/vstdnk/stackboards/EntranceActivity.kt
@@ -16,32 +16,7 @@ import com.vstdnk.stackboards.ui.theme.StackboardsTheme
 class EntranceActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            StackboardsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_entrance)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StackboardsTheme {
-        Greeting("Android")
     }
 }
