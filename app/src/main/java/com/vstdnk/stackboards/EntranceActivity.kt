d775d8f52ec0816269f730185619e30d6a37143c
Vladimir Stadnik <va.stdnk@gmail.com>
2024-07-30 22:30:16 +0300

    Updated

--- app/src/main/java/com/vstdnk/stackboards/EntranceActivity.kt
+++ app/src/main/java/com/vstdnk/stackboards/EntranceActivity.kt
@@ -1,6 +1,8 @@
 package com.vstdnk.stackboards
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +13,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.material.textfield.TextInputEditText
 import com.vstdnk.stackboards.ui.theme.StackboardsTheme
 
 class EntranceActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entrance)
+
 
     }
 }
